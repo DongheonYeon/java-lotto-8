2e5f979ca72528ae38efed4421b6297f816979cf
yeon2731 <kite2731@gmail.com>
2025-11-02 21:18:59 +0900

    feat(LottoStorage): implement LottoStorage domain

--- src/main/java/lotto/domain/LottoStorage.java
+++ src/main/java/lotto/domain/LottoStorage.java
@@ -1,7 +1,24 @@
 package lotto.domain;
 
+import lotto.domain.Lotto;
+
+import java.util.Collections;
+import java.util.List;
+
 public class LottoStorage {
+    private final List<Lotto> lottos;
+
+    public LottoStorage(List<Lotto> lottos) {
+        this.lottos = List.copyOf(lottos);
+    }
 
     // getLottos
+    public List<Lotto> getLottos() {
+        return Collections.unmodifiableList(lottos);
+    }
+
     // size
+    public int size() {
+        return lottos.size();
+    }
 }
